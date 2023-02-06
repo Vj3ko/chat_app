602de3f8bd934eb4311d0e2ca3aeac4993442e5b
Vjeko <dizdar.vjeko@gmail.com>
2023-02-06 10:05:45 +0100

    changed getRandomColor function cuz sometimes it was not getting color due to lack of characters for a color hex code

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import './Login.scss';
 
 function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 export default function Login({ setUser }) {
